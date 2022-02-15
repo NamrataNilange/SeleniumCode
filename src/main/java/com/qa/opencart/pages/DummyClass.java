1a96bc517bab6fd5e7afd5757859a1c20997be2a
NamrataNilange <namratanilange8@gmail.com>
2022-02-15 16:01:10 +0530

    New branch data commit

--- src/main/java/com/qa/opencart/pages/DummyClass.java
+++ src/main/java/com/qa/opencart/pages/DummyClass.java
@@ -5,6 +5,7 @@ public class DummyClass {
 	public static void main(String[] args) {
 		int data = 34;
 		String name = "sharda";
+		String name1 = "sharda1";
 		System.out.println("dummy class for github");
 	}
 
